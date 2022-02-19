5ee25656756be67f52b9d88985d85886494701d3
DBatterman <dbatterman24@gmail.com>
2022-02-18 20:29:16 -0800

    Adding Election Audit Code through 3.4

--- PyPoll.py
+++ PyPoll.py
@@ -1,15 +1,52 @@
 # Add our dependancies.
 import csv
 import os 
-# Assign a variable for the file to load and the path
+# Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources","election_results.csv")
-# Assign a variable to save the file to a path
+# Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#Open the election results and read the file.
+# Initialize total vote counter.
+total_votes = 0
+
+# Candidate Options and candidate votes.
+candidate_options = []
+# Declare the empty dictionary.
+candidate_votes = {}
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-    #Print the header row.
+
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    #Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate.
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to the candidate's count.
+        candidate_votes[candidate_name] += 1
+    
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate percentage of votes
+    vote_percentage = float(votes)/float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: recieved {vote_percentage:.1f}% of the vote.")
